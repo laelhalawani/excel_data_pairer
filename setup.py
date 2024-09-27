85f33329dc62d59e40fd532dfbd798cb23007f2d
laelhalawani <laelhalawani@gmail.com>
2024-09-27 15:19:36 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup, find_packages
 
+# Read the requirements from the requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='excel_navigator',  # Replace with your package name
     version='0.0.1',  # Initial version
     author='Lael Al-Halawani',
     author_email='laelhalawani@gmail.com',
-    description='Get',
+    description='Get data pairs from excel sheets, easily and automatically build jsons. Perfect for extracting translations from excel files.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/your-repo',  # Replace with your repo URL
+    url='https://github.com/laelhalawani/excel_data_pairer',  # Replace with your repo URL
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
-    install_requires=[
-        # List your package dependencies here
-        # 'some_package>=1.0.0',
-    ],
+    python_requires='>=3.10.14',
+    install_requires=requirements,
 )
